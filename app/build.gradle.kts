8bff8e4197ec9781fe5d2a64e8f2ddfb9d9467c5
shafnim64-dotcom <shafnim64@gmail.com>
2026-06-15 19:13:27 +0530

    Remove debug signing configuration
    Removed debug signing configuration from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
     }
-  }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
